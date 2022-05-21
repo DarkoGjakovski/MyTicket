5f7e3bca6297b2c346aab65fdfae3636c357baed
Darko Gjakovski <darkogj@iborn.net>
2022-05-21 14:08:14 +0200

    MyTicket first setup

---
+++ MyTicket/MyTicket.Domain/DomainModels/Order.cs
@@ -0,0 +1,16 @@
+﻿using MyTicket.Domain.Idenitity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTicket.Domain.DomainModels
+{
+    public class Order : BaseEntity
+    {
+        public string UserId { get; set; }
+        public MyTicketApplicationUser User { get; set; }
+
+        public IEnumerable<ProductInOrder> ProductInOrders { get; set; }
+    }
+}
---
+++ MyTicket/MyTicket.Repository/Implementation/UserRepository.cs
@@ -0,0 +1,65 @@
+﻿using MyTicket.Domain.Idenitity;
+using MyTicket.Repository.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyTicket.Repository.Implementation
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext context;
+        private DbSet<MyTicketApplicationUser> entities;
+        string errorMessage = string.Empty;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+            entities = context.Set<MyTicketApplicationUser>();
+        }
+        public IEnumerable<MyTicketApplicationUser> GetAll()
+        {
+            return entities.AsEnumerable();
+        }
+
+        public MyTicketApplicationUser Get(string id)
+        {
+            return entities
+               .Include(z => z.UserCart)
+               .Include("UserCart.ProductInShoppingCarts")
+               .Include("UserCart.ProductInShoppingCarts.Product")
+               .SingleOrDefault(s => s.Id == id);
+        }
+        public void Insert(MyTicketApplicationUser entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entities.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(MyTicketApplicationUser entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entities.Update(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(MyTicketApplicationUser entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entities.Remove(entity);
+            context.SaveChanges();
+        }
+    }
+}
